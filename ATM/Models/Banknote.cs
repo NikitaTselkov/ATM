d66e411e3cdac9d6897396c82b2496640e714e6c
Nikita <ncelkov@rit-it.com>
2023-07-08 07:37:27 +0700

    Configured data model. Added class Cassette and struct Banknote.

---
+++ ATM/Models/Banknote.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATM.Models
+{
+    public struct Banknote
+    {
+        /// <summary>
+        /// Номинал.
+        /// </summary>
+        public int Denomination { get; init; }
+
+        public Banknote(int denomination)
+        {
+            Denomination = denomination;
+        }
+    }
+}
---
+++ ATM/Models/Cassette.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ATM.Models
+{
+    public sealed class Cassette
+    // 4 - 6 в 1 кассете 2500 банкнот в каждую касету можно добавить касету только своего номинала
+    // Банкомат сохраняет информацию по каждой операции
+    // Дополнительно банкомат пересчитывает все купюры перед выдачей
+    {
+        private Stack<Banknote> _banknotes;
+        public Stack<Banknote> Banknotes
+        {
+            get { return _banknotes; }
+            private set
+            {
+                if (value.Count > _maxCountOfBanknotes)
+                    throw new ArgumentOutOfRangeException($"Count of Banknotes cannot be more than {_maxCountOfBanknotes}");
+                _banknotes = value; 
+            }
+        }
+
+        private int _countOfBanknotes;
+        public int CountOfBanknotes
+        {
+            get { return _countOfBanknotes; }
+            private set { _countOfBanknotes = Banknotes.Count; }
+        }
+
+
+        private const int _maxCountOfBanknotes = 2500;
+
+
+        public Cassette(Stack<Banknote> banknotes)
+        {
+            Banknotes = banknotes;
+        }
+
+
+        public void AddBanknote(Banknote banknote)
+        {
+            if (Banknotes.Count > 0 && Banknotes.First().Denomination != banknote.Denomination)
+                throw new FormatException("Banknotes must be of the same denomination");
+
+            Banknotes.Push(banknote);
+        }
+
+        public void RemoveBanknote(int countOfBanknotes)
+        {
+            if (countOfBanknotes < 0)
+                throw new ArgumentOutOfRangeException("countOfBanknotes cannot be less than 0");
+
+            if (countOfBanknotes > Banknotes.Count)
+                throw new ArgumentOutOfRangeException("Not enough banknotes to dispense");
+
+            for (int i = 0; i < countOfBanknotes; i++)
+                Banknotes.Pop();
+        }
+
+    }
+}
