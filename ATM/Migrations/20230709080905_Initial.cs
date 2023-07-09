c3cccb25e3be0ec68682344bbd8760eabe2ffc33
Nikita <ncelkov@rit-it.com>
2023-07-09 15:22:00 +0700

    Added Migration

---
+++ ATM/Migrations/20230709080905_Initial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ATM.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cassettes",
+                columns: table => new
+                {
+                    Denomination = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CountOfBanknotes = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cassettes", x => x.Denomination);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cassettes");
+        }
+    }
+}
